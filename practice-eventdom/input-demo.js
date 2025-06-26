d9013b3576ff508dfdef488e5855ec754c428ca9
G578412025 <257841@st.takushoku-u.ac.jp>
2025-06-26 14:51:07 +0900

    練習4-9

--- practice-eventdom/input-demo.js
+++ practice-eventdom/input-demo.js
@@ -7,6 +7,6 @@ b.addEventListener('click', greeting);
 
 function greeting() {
     shimei = input.value;
-    let aisatsu  = "こんにちは" + shimei;
+    let aisatsu  = "こんにちは," + shimei + "さん";
     p.textContent = aisatsu;
 }
