d0944b1b81840d73a910ab3ac1488d0496247f18
G578412025 <257841@st.takushoku-u.ac.jp>
2025-07-17 16:51:54 +0900

    第6回感想以外

--- tv-program.js
+++ tv-program.js
@@ -13,7 +13,9 @@ const genre = [
   { value: "1100", text: "福祉" }
 ];
 const select_junre = document.querySelector('select#genre');
+const select_service = document.querySelector("select#service");
 
+//チャンネルとジャンルのプルダウンメニューを作成
 genre.forEach(item =>{
   const option = document.createElement('option');
   option.value = item.value;
@@ -21,39 +23,48 @@ genre.forEach(item =>{
   select_junre.appendChild(option);
 });
 
-// 課題3-2 のプログラムはこの関数の中に記述すること
+// 課題3-2 のプログラムはこの関数の中に記述する(想定していたJsonの形が違ったため使用不能)
 function print(data) {
- const list =  data.list.g1;
- for(const table of list){
-  console.log('開始時刻 : ' + table.start_time);
-  console.log('終了時刻 : ' + table.end_time);
-  console.log('チャンネル : ' + table.service.name);
-  console.log('タイトル : ' + table.title);
-  console.log('サブタイトル : ' + table.subtitle);
-  console.log('番組説明 : ' + table.content);
-  console.log('出演者 : ' + table.act);
- }
+  for(const table of data){
+    console.log('開始時刻 : ' + table.start_time);
+    console.log('終了時刻 : ' + table.end_time);
+    console.log('チャンネル : ' + table.service.name);
+    console.log('タイトル : ' + table.title);
+    console.log('サブタイトル : ' + table.subtitle);
+    console.log('番組説明 : ' + table.content);
+    console.log('出演者 : ' + table.act);
+  }
 }
 
 // 課題5-1 の関数 printDom() はここに記述すること
-const button = document.querySelector("button#submit");
-const select_service = document.querySelector("select#service");
-button.addEventListener('click',printDom);
-const tableHeader = ["開始時刻","放映時間","チャンネル","タイトル","サブタイトル","番組説明","出演者"];
-
 function printDom(data) {
-  const list =  data.list.g1;
-  const service = select_service.value;
-  const junre = select_junre.value;
+  //dataから必要情報をlistに格納し、確認のためconsoleに出力(dataがnullなら空にする)
+  let list = "";
+  let service = select_service.value;
+  if(service === "g1"){
+    list = data.list.g1;
+  }else if(service === "e1"){
+    list = data.list.e1;
+  }
+  console.log(data);
+  console.log(list);
 
-  console.log("チャンネル:" + service);
-  console.log("ジャンル:" + junre);
+  //createTableでtableエレメントを作成
+  const table = createTableElement(list);  
 
+  //tableをbodyに入れる
+  const body = document.querySelector('body');
+  body.insertAdjacentElement('beforeend',table);
+}
+
+//受け取ったdata.list.{genre}のJsonからtableエレメントを作成し返す関数
+const tableHeader = ["開始時刻","放映時間","チャンネル","タイトル","サブタイトル","番組説明","出演者"];
+function createTableElement(list){
   //テーブルの枠組みを作成
   const table = document.createElement("table");
   table.setAttribute("border",1);
   
-  //テーブルのヘッダーを設定
+  //テーブルのヘッダーを作成して設定
   const thead = document.createElement('thead');
   let tr = document.createElement('tr');
   for(let head of tableHeader){
@@ -65,13 +76,13 @@ function printDom(data) {
   thead.insertAdjacentElement('beforeend',tr);
   table.insertAdjacentElement('beforeend',thead);
 
-  //テーブルの中身を設定(div以下は1行ごとに設定)
+  //テーブルの中身を設定
   const tbody = document.createElement('tbody');
   for(let row of list){
     tr = document.createElement('tr');
     
     //開始時間と放映時間のみ個別で記載
-    let start = formateISO(row.start_time);
+    let start = formatISO(row.start_time);
     td = document.createElement('td');
     td.textContent = start;
     tr.insertAdjacentElement('beforeend',td);
@@ -99,12 +110,24 @@ function printDom(data) {
 
   table.insertAdjacentElement('beforeend',tbody);
 
-  const body = document.querySelector('body');
-  body.insertAdjacentElement('beforeend',table);
+  return table;
+}
+
+//tableまたはエラーメッセージが作成済みならそれを破棄する関数
+function deleteDynamicElement(){
+  let ck = document.querySelector("table");
+  if(ck !== null){
+    ck.remove();
+  }
+
+  ck = document.querySelector("p#err");
+  if(ck !== null){
+    ck.remove();
+  }
 }
 
 //ISO標準時形式を成形する
-function formateISO(isoString){
+function formatISO(isoString){
   const date = new Date(isoString);
 
   // 各要素を取得し、2桁表示に整形
@@ -133,18 +156,53 @@ function timeCalc(start_time,end_time){
 }
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-
+const button = document.querySelector("button#submit");
+button.addEventListener('click',sendRequest);
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  //前に作成した動的要素が存在するなら削除
+  deleteDynamicElement();
+
+  //serviceとgenreを取得し、目標URLを作成
+  let service = select_service.value;
+  let genre = select_junre.value;
+
+  let url = "https://www.nishita-lab.org/web-contents/jsons/nhk/" + service + "-" + genre + "-j.json";
+
+  //通信
+  axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
+
 
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
+//成功時応答文章を丸々printDomに送付しテーブルを作成させる
+//Jsonそのままで帰ってくるためJson化は不要
 function showResult(resp) {
+  console.log("通信成功");
+	let data = resp.data;
 
+  //data.listが存在するならtableを作成,そうでないならエラーメッセージをDomに表示
+  if(data.list !== null){
+    printDom(data);
+  }else{
+    errPrintDom();
+  }
+}
+
+//エラーが起きたことを利用者に通知
+function errPrintDom(){
+  let m = document.createElement("p");
+  m.setAttribute("id","err");
+  m.textContent = "エラーが発生しました。もう一度やり直すか検索条件を変更してください。";
+  let b = document.querySelector("body");
+  b.insertAdjacentElement("beforeend",m);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -156,89 +214,3 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
-
-////////////////////////////////////////
-// 以下はテレビ番組表のデータサンプル
-// 注意: 第5回までは以下を変更しないこと！
-// 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "list": {
-    "g1": [
-      {
-        "id": "2022030428673",
-        "event_id": "28673",
-        "start_time": "2022-03-04T04:35:00+09:00",
-        "end_time": "2022-03-04T04:40:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "みんなのうた「ごっつぉさま」／「超変身！ミネラルフォーマーズ」",
-        "subtitle": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "content": "「ごっつぉさま」うた：須貝智郎／「超変身！ミネラルフォーマーズ」うた：鬼龍院翔ｆｒｏｍゴールデンボンバー",
-        "act": "",
-        "genres": [
-          "0409",
-          "0700",
-          "0504"
-        ]
-      },
-      {
-        "id": "2022030427069",
-        "event_id": "27069",
-        "start_time": "2022-03-04T23:05:00+09:00",
-        "end_time": "2022-03-04T23:10:00+09:00",
-        "area": {
-          "id": "130",
-          "name": "東京"
-        },
-        "service": {
-          "id": "g1",
-          "name": "ＮＨＫ総合１",
-          "logo_s": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-100x50.png",
-            "width": "100",
-            "height": "50"
-          },
-          "logo_m": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x100.png",
-            "width": "200",
-            "height": "100"
-          },
-          "logo_l": {
-            "url": "//www.nhk.or.jp/common/img/media/gtv-200x200.png",
-            "width": "200",
-            "height": "200"
-          }
-        },
-        "title": "パラスポーツ×アニメ「アニ×パラ」▽パラアルペンスキーテーマ曲江口寿史×ＡＣＣ",
-        "subtitle": "パラスポーツの魅力をアニメで伝える番組。高速滑走に挑む精神力が試されるパラアルペンスキーを描く。キャラ原案：江口寿史／曲：Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ",
-        "content": "パラスポーツの魅力をアニメで伝えるプロジェクトの第１３弾。圧倒的なスピードに挑む「パラアルペンスキー」の世界を江口寿史原案の魅力的なキャラクターで描く。平昌パラリンピック金メダリストの村岡桃佳選手への取材から生まれた主人公・桃は、スピードへの恐怖を克服していく。その壁を越えた先にあるものとは…　テーマ曲　♪「Ｏｎ　Ｙｏｕｒ　Ｍａｒｋ」はＡｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂが手掛けた。",
-        "act": "【声】松本まりか，【出演】Ａｗｅｓｏｍｅ　Ｃｉｔｙ　Ｃｌｕｂ，【監督】西村一彦，【脚本】加納新太，【原案】江口寿史",
-        "genres": [
-          "0700"
-        ]
-      }
-    ]
-  }
-};
-
